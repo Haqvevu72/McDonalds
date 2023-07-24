3ef50210f3b6c70a9cbc0c34ae5a02781b89d3d8
Haqve_vu72 <Haqve_vu72@itstep.edu.az>
2023-07-24 10:21:27 +0400

    35%

--- WpfApp1/View/LogIn.xaml.cs
+++ WpfApp1/View/LogIn.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,15 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApp1.Model;
 using WpfApp1.ViewModel;
-
+using System.IO;
+using System.Configuration;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using System.Threading;
 namespace WpfApp1.View
 {
     /// <summary>
@@ -21,18 +29,42 @@ namespace WpfApp1.View
     public partial class LogIn : Window
     {
         public ICommand? _login { get; set; }
+        public ICommand? _register { get; set; }
+        static ObservableCollection<User> _users;
+
+
         public LogIn()
         {
             InitializeComponent();
             DataContext = this;
-            _login = new RelayCommand(exe_log,can_exe_log);
+            _login = new RelayCommand(exe_log, can_exe_log);
+            _register = new RelayCommand(exe_reg, can_exe_log);
+            string txt_users = File.ReadAllText("C:\\Users\\Elgun\\Source\\Repos\\McDonalds\\WpfApp1\\JSON Files\\Users.json");
+            _users = JsonConvert.DeserializeObject<ObservableCollection<User>>(txt_users);
         }
 
-        public void exe_log(object? parameter) 
+        public void exe_log(object? parameter)
         {
-            
+
+            for (int i = 0; i < _users.Count; i++)
+            {
+                if (Username.Text == _users[i].Username && Password.Password == _users[i].Password)
+                {
+                    LogInFrame.Navigate(new Logged());
+                    break;
+                }
+                LogInFrame.Navigate(new Fail());
+            }
         }
-        public bool can_exe_log(object? parameter) 
+        public void exe_reg(object? parameter)
+        {
+            _users.Add(new User(Username.Text, Password.Password));
+            string txt_reg = JsonConvert.SerializeObject(_users);
+            File.WriteAllText("C:\\Users\\Elgun\\Source\\Repos\\McDonalds\\WpfApp1\\JSON Files\\Users.json", txt_reg);
+            LogInFrame.Navigate(new Registered());
+            LogInFrame.Navigate(new LogIn());
+        }
+        public bool can_exe_log(object? parameter)
         {
             if (Username.Text != "" && Password.Password != "") { return true; }
             return false;
